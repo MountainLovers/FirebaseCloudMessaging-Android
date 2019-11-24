9808a841992abf58a632fec3fc9d77614d9c6b90
MountainLovers <zzjzxh0870@gmail.com>
2019-11-25 02:46:37 +0800

    Revert "appsphinx-v1.0"
    This reverts commit 72ec2aae50e0edec590508289874a8d8a7525f09.

--- app/src/main/java/com/example/fcm/ProtocolClass.java
+++ app/src/main/java/com/example/fcm/ProtocolClass.java
@@ -2,22 +2,14 @@ package com.example.fcm;
 
 import android.util.Log;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.spongycastle.asn1.nist.NISTNamedCurves;
 import org.spongycastle.math.ec.ECCurve;
 import org.spongycastle.math.ec.ECPoint;
 import org.spongycastle.util.encoders.Hex;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.Serializable;
 import java.math.BigInteger;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.security.SecureRandom;
-import java.util.Scanner;
 
 public class ProtocolClass {
 
@@ -30,9 +22,7 @@ public class ProtocolClass {
 
     public static ECPoint alpha = null;
     public static ECPoint beta = null;
-    public static BigInteger k = null;
-
-    private static final String AUTH_KEY = "key=AAAAjGipi60:APA91bExp6Dw6I33EZ-noe8UgeL3I06m2dLyINsIS6C835DSfzS6R8dJroE0cL31JEyDeHncYsHI-tRkWykB0Ji7aZimiRfhxmj_J9jb8cGBQiiyCgiKDACfc750Ffzrz_tMlEzP6ife";
+    public static BigInteger k;
 
 
     public static ECPoint retrivePoint(String x, String y) {
@@ -51,10 +41,7 @@ public class ProtocolClass {
     public static ECPoint computeBeta(ECPoint alpha, BigInteger k) {
         assert alpha != null;
         assert k != null;
-        ECPoint beta = alpha.multiply(k);
-        Log.d("DEBUG", alpha.toString());
-        Log.d("DEBUG", beta.toString());
-        return beta;
+        return alpha.multiply(k);
     }
 
     public static BigInteger genK() {
@@ -71,67 +58,5 @@ public class ProtocolClass {
         } while (randk.equals(BigInteger.ZERO) || randk.compareTo(n) >= 0);
         Log.d(TAG, "Generate k:" + randk.toString(16));
         return randk;
-    }
-
-    public static void sendMsgToClient(String type, JSONObject payload) {
-        assert ProtocolClass.clientToken != null;
-        JSONObject jPayload = new JSONObject();
-        JSONObject jData = new JSONObject();
-        try {
-            jData.put("type", type);
-            jData.put("payload", payload);
-//            switch(type) {
-//                case "token":
-//                    assert ProtocolClass.deviceToken != null;
-//                    jData.put("type", "token");
-//                    jData.put("token", ProtocolClass.deviceToken);
-//                    break;
-//                case "ecpoint":
-//                    jData.put("type", "ecpoint");
-//                    jData.put("x", x);
-//                    jData.put("y", y);
-//                    jData.put("description", desp);
-//                    break;
-//                default:
-//                    jData.put("type", "unknown");
-//            }
-
-//            jPayload.put("priority", "high");
-            jPayload.put("data", jData);
-            jPayload.put("to", ProtocolClass.clientToken);
-
-            URL url = new URL("https://fcm.googleapis.com/fcm/send");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Authorization", AUTH_KEY);
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setDoOutput(true);
-
-            conn.connect();
-            Log.d("NETWORK", jPayload.toString());
-
-            // Send FCM message content.
-            OutputStream outputStream = conn.getOutputStream();
-            outputStream.write(jPayload.toString().getBytes());
-
-            if (conn.getResponseCode() == 200) {
-                Log.d("NETWORK", "POST SUCC: "+conn.getResponseMessage());
-            } else {
-                Log.d("NETWORK", "POST FAIL: "+conn.getResponseCode());
-            }
-
-            // Read FCM response.
-//            InputStream inputStream = conn.getInputStream();
-//            final String resp = convertStreamToString(inputStream);
-//
-//            Log.d(TAG, "send msg to client return value: "+resp);
-        } catch (JSONException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static String convertStreamToString(InputStream is) {
-        Scanner s = new Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next().replace(",", ",\n") : "";
     }
 }
